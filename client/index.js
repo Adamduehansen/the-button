da034ed0d67d68f14ddd19134c577907d8e6726e
Adam Due Hansen <adamduehan@gmail.com>
2018-04-16 22:56:56 +0200

    Adds styles to page.

--- client/index.js
+++ client/index.js
@@ -2,8 +2,20 @@ import io from "socket.io-client";
 
 require("./index.scss");
 
+const nameInput = document.querySelector(".name-input");
+const theButton = document.querySelector(".the-button");
+
+// Add an event listener on the button.
+theButton.addEventListener("click", () => {
+  nameInput.focus(); // Set focus back to name input on click.
+});
+
 const socket = io("http://localhost:3000/");
 
 socket.on("connection", () => {
   console.log("Connected!");
+});
+
+socket.on("connect_error", () => {
+  console.log("Ouch!");
 });
