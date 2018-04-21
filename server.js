d76fffb8e20d83bb606bd8847255bea0198b0bce
Adam Due Hansen <adamduehan@gmail.com>
2018-04-21 17:39:45 +0200

    Express server now serves all static files.

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 /* global __dirname process */
 const path = require("path");
-const express = require("express")();
-const server = require("http").Server(express);
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 const port = process.env.port || 3000;
 
 let numberOfButtonClicks = 0;
 
-// Routes
-express.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "wwwroot/index.html"));
-});
-express.get("/build.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "wwwroot/build.js"));
-});
+app.use(express.static(path.resolve(__dirname, "wwwroot")));
 
 /**
  * Handles the "disconnect" event.
